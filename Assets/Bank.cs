2bee171bd7f2155c476ab4424956e905a47bd783
MinSun Park <minsunp@andrew.cmu.edu>
2018-02-15 22:16:21 -0500

    Added UI & store money

---
+++ Assets/Bank.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bank : MonoBehaviour {
+
+    public bool loan_allowed;
+    public int loan;
+    public int interest_rate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -1,27 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Store : MonoBehaviour {
 
-	//public List Items;
+	/*   Variables for Store   */
+
+	public int money; // current money
 	public bool insurance;
-	//public List Equipment;
 	public int savings_account;
 	public int checkings_account;
+	public int productivity;
+	public int reputation;
+	//public List Items;
+	//public List Equipment;
 	//public string name;
+	//public string type;
 
-	int productivity;
-	string type;
-	int reputation;
+	/*   Variables from other scripts   */
+
+	Text score;
+
+	void Awake() {
+		money = 0;
+		insurance = false;
+		savings_account = 0;
+		checkings_account = 0;
+		productivity = 50;
+		reputation = 0;
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		// Initialize and display money
+		score = GameObject.Find("Money").GetComponent<Text>();
+		score.text = money.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		money = 10;
+		score.text = money.ToString();
 	}
 }
