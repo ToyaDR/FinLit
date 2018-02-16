a55a9bdc5df7b2500015ab01cd8a3c2c45521f3c
ToyaDR <vdr@andrew.cmu.edu>
2018-02-15 21:09:33 -0500

    Add ability to save and load Game

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Game {
+
+	public static Game current;
+	public Store my_store;
+	//public Bank my_bank;
+
+	public Game() {
+		my_store = new Store ();
+		//my_bank = new Bank ();
+
+	}
+}
---
+++ Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SaveLoad {
+	public static Game savedGame = new Game();
+
+	private static string save_path = Path.Combine(Application.persistentDataPath, "/savedGame.gd");
+
+	public static void Save() {
+		savedGame = Game.current;
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (save_path);
+		bf.Serialize (file, SaveLoad.savedGame);
+		file.Close ();
+	}
+
+	public static void Load() {
+		if (File.Exists (save_path)) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (save_path, FileMode.Open);
+			SaveLoad.savedGame = (Game) bf.Deserialize(file);
+			file.Close();
+		}
+	}
+}
