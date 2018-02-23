971432c127d9b042511c60be4c65156208ab3a32
MinSun Park <minsunp@andrew.cmu.edu>
2018-02-22 21:18:22 -0500

    Added assets & 2 employees

---
+++ Assets/Scripts/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Employee {
+
+    private string emp_name;
+    private int emp_morale;
+
+	public void setName(string name) {
+        emp_name = name;
+    }
+
+    public void setMorale(int morale) {
+        emp_morale = morale;
+    }
+
+    public string getName() {
+        return emp_name;
+    }
+
+    public int getMorale() {
+        return emp_morale;
+    }
+}
