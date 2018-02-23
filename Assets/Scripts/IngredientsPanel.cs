61899573f18ae2db42d88c7c7c2fe8ea54acd34f
ToyaDR <vdr@andrew.cmu.edu>
2018-02-23 14:24:07 -0500

    Ingredients panel is programmatically populated

--- Assets/Scripts/IngredientsPanel.cs
+++ Assets/Scripts/IngredientsPanel.cs
@@ -6,27 +6,40 @@ using UnityEngine.UI;
 public class IngredientsPanel : MonoBehaviour {
 
 	private ArrayList ingredients;
+	public Canvas canvas;
+	Vector2 anchorPoint;
 
 	// Use this for initialization
 	void Start () {
+		float canvasLeft = canvas.GetComponent<RectTransform> ().anchorMin.x;
+		float canvasTop = canvas.GetComponent<RectTransform> ().anchorMax.y;
+
+
 		ingredients = new ArrayList ();
 
 		Ingredient flour = new Ingredient ();
-		flour.ing_name = "flour";
+		flour.ing_name = "Flour";
 		flour.ing_price = 2;
 
 		ingredients.Add(flour);
 
 		Ingredient eggs = new Ingredient ();
-		eggs.ing_name = "eggs";
+		eggs.ing_name = "Eggs";
 		eggs.ing_price = 1;
 
 		ingredients.Add(eggs);
 
+		float offset = 0;
 		foreach(Ingredient i in ingredients){
 			GameObject ingTextGO = Instantiate (Resources.Load ("Ingredients", typeof(GameObject))) as GameObject;
 			ingTextGO.transform.SetParent (transform);
-			ingTextGO.GetComponent<Text> ().text = i.ing_name + " " + i.ing_price;
+			ingTextGO.GetComponent<Text> ().text = i.ing_name + " $" + i.ing_price;
+
+
+			anchorPoint = new Vector2 (canvasLeft, canvasTop + offset);
+
+			ingTextGO.GetComponent<Text> ().rectTransform.anchoredPosition = new Vector2 (canvasLeft, canvasTop + offset);
+			offset += ingTextGO.GetComponent<RectTransform>().rect.height;
 		}
 	}
 	
