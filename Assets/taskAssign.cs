584f9fb101073c724671e2e7ac3e23b7fcd6b610
MinSun Park <minsunp@andrew.cmu.edu>
2018-03-01 14:02:09 -0500

    UI for task assigning panel done

--- Assets/taskAssign.cs
+++ Assets/taskAssign.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class taskAssign : MonoBehaviour {
+public class TaskAssign : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
