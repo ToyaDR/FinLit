61b3adb62d78a6e5eea4d63ef275e07c01ccdba7
ToyaDR <vdr@andrew.cmu.edu>
2018-04-19 00:03:25 -0400

    Start Screen

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartManager: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadBizworks(){
+		/* Called on start press */
+		SceneManager.LoadScene (1);
+	}
+}
