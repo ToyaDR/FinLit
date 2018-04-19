7f12c0c96386ccc90b1ae791e4e1741ee458d20b
ToyaDR <vdr@andrew.cmu.edu>
2018-04-19 00:29:36 -0400

    Credits scene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class StartManager: MonoBehaviour {
+public class LevelManager: MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -15,8 +15,21 @@ public class StartManager: MonoBehaviour {
 		
 	}
 
+	public void LoadMenu() {
+		SceneManager.LoadScene (0);	
+	}
+
 	public void LoadBizworks(){
 		/* Called on start press */
 		SceneManager.LoadScene (1);
+	}
+
+	public void LoadCredits(){
+		/* Called on credits press */
+		SceneManager.LoadScene (2);
+	}
+
+	public void Exit(){
+		Application.Quit ();
 	}
 }
